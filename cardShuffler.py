508d3be7821c631ffb8c8cb410c1f3a6c919a51e
BillyF-2005 <billforsey@gmail.com>
2024-08-04 15:00:24 +0100

    Add files via upload

---
+++ cardShuffler.py
@@ -0,0 +1,20 @@
+from operator import index
+import random
+
+
+
+def shuffle(i):
+    suits = ['club', 'diamond', 'heart', 'spade']
+    values = [2,3,4,5,6,7,8,9,10,11,12,13,14]
+    faces = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
+
+    cardSuit = random.choice(suits)
+    cardValue = random.choice(values)
+    cardFace = faces[values.index(cardValue)]
+    card = [cardSuit,cardFace,cardValue]
+    return card[i]
+    
+card1 = [shuffle(0),shuffle(1),shuffle(2)]
+    
+
+
